b227869c274f213d5a506b6d987444555a148dc5
Kingbo Lu <ambudao@gmail.com>
2013-04-22 01:12:40 +0800

    Put all mail API functions in mail_client.

--- src/imapc.erl
+++ src/imapc.erl
@@ -2,34 +2,30 @@
 
 -include("imap.hrl").
 
--behaviour(gen_server).
-
--export([open_account/5, close_account/1,
+-export([connect/2, connect/3, login/3,
          list/3, status/3, select/2, examine/2, append/4, expunge/1,
-         search/2, fetch/3, store/4, copy/3
+         search/2, fetch/3, store/4, copy/3,
+         noop/1, logout/1, disconnect/1, quit/1,
+         fsm_state/1
         ]).
 
--export([fsm_state/1]).
-
--export([init/1, handle_call/3, handle_cast/2, handle_info/2, code_change/3,
-         terminate/2]).
 
 %%%-----------------
 %%% Client functions
 %%%-----------------
+connect(Host, Port) ->
+  connect(Host, Port, []).
+connect(Host, Port, Options) ->
+  case lists:member(ssl, Options) of 
+    true -> gen_fsm:start_link(imapc_fsm, {ssl, Host, Port}, []);
+    false -> gen_fsm:start_link(imapc_fsm, {tcp, Host, Port}, [])
+  end.
 
-open_account(ConnType, Host, Port, User, Pass) ->
-  TrapexitFlag = proplists:get_value(trap_exit,erlang:process_info(self())),
-  process_flag(trap_exit, true), 
-  Ret = gen_server:start_link(?MODULE, {ConnType, Host, Port, User, Pass}, []), 
-  process_flag(trap_exit, TrapexitFlag), 
-  Ret.
+login(Fsm, User, Pass) ->
+  gen_fsm:sync_send_event(Fsm, {command, login, {User, Pass}}).
 
-close_account(Account) ->
-  gen_server:call(Account, close_account).
-
-list(Account, RefName, Mailbox) ->
-  gen_server:call(Account, {list, RefName, Mailbox}).
+list(Fsm, RefName, Mailbox) ->
+  gen_fsm:sync_send_event(Fsm, {command, list, [RefName, imapc_util:quote_mbox(Mailbox)]}).
 
 %% @link http://tools.ietf.org/html/rfc3501#section-6.3.10
 %% The currently defined status data items that can be requested are:
@@ -38,109 +34,58 @@ list(Account, RefName, Mailbox) ->
 %%   UIDNEXT       --->  The next unique identifier value of the mailbox.
 %%   UIDVALIDITY   --->  The unique identifier validity value of the mailbox.
 %%   UNSEEN        --->  The number of messages which do not have the \Seen flag set.
-status(Account, Mailbox, StatusDataItems) ->
-  gen_server:call(Account, {status, Mailbox, StatusDataItems}).
+status(Fsm, Mailbox, StatusDataItems) ->
+  gen_fsm:sync_send_event(Fsm, {command, status, [imapc_util:quote_mbox(Mailbox), StatusDataItems]}).
 
-select(Account, Mailbox) ->
-  {ok, M} = gen_server:call(Account, {select, Mailbox}),
-  {ok, M#mailbox{name = string:to_upper(Mailbox)}}.
+select(Fsm, Mailbox) ->
+  {ok, M} = gen_fsm:sync_send_event(Fsm, {command, select, imapc_util:quote_mbox(Mailbox)}),
+  {ok, M#mailbox{name = Mailbox}}.
 
-examine(Account, Mailbox) ->
-  gen_server:call(Account, {examine, Mailbox}).
+examine(Fsm, Mailbox) ->
+  gen_fsm:sync_send_event(Fsm, {command, examine, Mailbox}).
 
 %% "From: Fred Foobar <foobar@Blurdybloop.COM>\r\nSubject: afternoon meeting\r\n\r\nddd\r\n"
-append(Account, Mailbox, Flags, Message) ->
-  gen_server:call(Account, {append, Mailbox, Flags, Message}).
+append(Fsm, Mailbox, Flags, Message) ->
+  gen_fsm:sync_send_event(Fsm, {command, append, [Mailbox, Flags, Message]}).
 
-expunge(Account) ->
-  gen_server:call(Account, expunge).
+expunge(Fsm) ->
+  gen_fsm:sync_send_event(Fsm, {command, expunge, []}).
 
-search(Account, SearchKeys) ->
-  gen_server:call(Account, {search, SearchKeys}).
+search(Fsm, SearchKeys) ->
+  gen_fsm:sync_send_event(Fsm, {command, search, SearchKeys}).
 
-fetch(Account, SequenceSet, MsgDataItems) ->
-  gen_server:call(Account, {fetch, SequenceSet, MsgDataItems}, infinity).
+fetch(Fsm, SequenceSet, MsgDataItems) ->
+  gen_fsm:sync_send_event(Fsm, {command, fetch, [SequenceSet, MsgDataItems]}, infinity).
 
-copy(Account, SequenceSet, Mailbox) ->
-  gen_server:call(Account, {copy, SequenceSet, Mailbox}).
+copy(Fsm, SequenceSet, Mailbox) ->
+  gen_fsm:sync_send_event(Fsm, {command, copy, [SequenceSet, Mailbox]}).
 
-store(Account, SequenceSet, Flags, Action) ->
-  gen_server:call(Account, {store, SequenceSet, Flags, Action}).
+store(Fsm, SequenceSet, Flags, Action) ->
+  gen_fsm:sync_send_event(Fsm, {command, store, [SequenceSet, Flags, Action]}).
 
-fsm_state(Account) ->
-  gen_server:call(Account, fsm_state).
+logout(Fsm) ->
+  gen_fsm:sync_send_event(Fsm, {command, logout, {}}).
+
+disconnect(Fsm) ->
+  gen_fsm:sync_send_all_state_event(Fsm, {command, disconnect, {}}).
+
+noop(Fsm) ->
+  gen_fsm:sync_send_event(Fsm, {command, noop, {}}).
+
+fsm_state(Fsm) ->
+  gen_fsm:sync_send_all_state_event(Fsm, fsm_state).
+
+quit(Fsm) ->
+  ok = logout(Fsm),
+  ok = disconnect(Fsm).
+
 %%%-------------------
 %%% Callback functions
 %%%-------------------
-
-init({ConnType, Host, Port, User, Pass}) ->
-  try
-    {ok, Conn} = case ConnType of
-                   tcp -> imapc_fsm:connect(Host, Port);
-                   ssl -> imapc_fsm:connect_ssl(Host, Port)
-                 end,
-    ok = imapc_fsm:login(Conn, User, Pass),
-    {ok, Conn}
-  catch
-    error:{badmatch, {error, Reason}} -> {stop, Reason}
-  end.
-
-handle_call(close_account, _From, Conn) ->
-  try
-    ok = imapc_fsm:logout(Conn),
-    ok = imapc_fsm:disconnect(Conn),
-    {stop, normal, ok, Conn}
-  catch
-    error:{badmatch, {error, Reason}} -> {stop, Reason, {error, Reason}, Conn}
-  end;
-  
-handle_call({list, RefName, Mailbox}, _From, Conn) ->
-  {reply, imapc_fsm:list(Conn, RefName, Mailbox), Conn};
-handle_call({status, Mailbox, StatusDataItems}, _From, Conn) ->
-  {reply, imapc_fsm:status(Conn, Mailbox, StatusDataItems), Conn};
-handle_call({select, Mailbox}, _From, Conn) ->
-  {reply, imapc_fsm:select(Conn, Mailbox), Conn};
-handle_call({examine, Mailbox}, _From, Conn) ->
-  {reply, imapc_fsm:examine(Conn, Mailbox), Conn};
-handle_call({append, Mailbox, Flags, Message}, _From, Conn) ->
-  {reply, imapc_fsm:append(Conn, Mailbox, Flags, Message), Conn};
-handle_call(expunge, _From, Conn) ->
-  {reply, imapc_fsm:expunge(Conn), Conn};
-handle_call({search, SearchKeys}, _From, Conn) ->
-  {reply, imapc_fsm:search(Conn, SearchKeys), Conn};
-handle_call({fetch, SequenceSet, MsgDataItems}, _From, Conn) ->
-  {reply, imapc_fsm:fetch(Conn, SequenceSet, MsgDataItems), Conn};
-handle_call({copy, SequenceSet, Mailbox}, _From, Conn) ->
-  {reply, imapc_fsm:copy(Conn, SequenceSet, Mailbox), Conn};
-handle_call({store, SequenceSet, Flags, Action}, _From, Conn) ->
-  {reply, imapc_fsm:store(Conn, SequenceSet, Flags, Action), Conn};
-handle_call(fsm_state, _From, Conn) ->
-  {reply, imapc_fsm:fsm_state(Conn), Conn};
-handle_call(_, _From, Conn) ->
-  {reply, ignored, Conn}.
-
-
-
-handle_cast(_Msg, State) ->
-    {noreply, State}.
-
-handle_info(_Info, State) ->
-    {noreply, State}.
-
-code_change(_OldVsn, State, _Extra) ->
-    {ok, State}.
-
-terminate(normal, _State) ->
-  ok;
-terminate(Reason, _State) ->
-  {error, Reason}.
-
-%%%-----------
-%%% tests
-%%%-----------
-
--ifdef(TEST).
--include_lib("eunit/include/eunit.hrl").
-
-
--endif.
+% init({ConnType, Host, Port, User, Pass, Options}) ->
+%   try
+%     ok = ,
+%     {ok, Conn}
+%   catch
+%     error:{badmatch, {error, Reason}} -> {stop, Reason}
+%   end.
